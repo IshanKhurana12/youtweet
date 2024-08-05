ff644935be70ad0850c3990a12dd918bf5ad4c3e
ishan2op <ishankhurana176@gmail.com>
2024-08-06 02:22:38 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+import { User } from "./user.model";
+
+
+const subscriptionSchema=new mongoose.Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true});
+
+
+
+export const Subscription=mongoose.model("Subscription",subscriptionSchema);
