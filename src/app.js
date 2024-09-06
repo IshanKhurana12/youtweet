d242812a3589f44bc8ceb66cb2b4d4d8e888384b
ishan2op <ishankhurana176@gmail.com>
2024-09-06 17:14:41 +0530

    cors options added

--- src/app.js
+++ src/app.js
@@ -5,9 +5,24 @@ import helmet from "helmet";
 
 const app=express();
 
+const allowedOrigins = ['http://localhost:5173', 'https://youtweet.onrender.com'];
+
+// CORS configuration
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+};
+
 
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json({limit:"16kb"}))
 
